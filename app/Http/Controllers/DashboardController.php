33123846e53a3d77cb98802d3e4008156c7bdff9
Clifford Mwale <cliffmwale97@gmail.com>
2022-09-02 07:38:17 +0200

    Commit

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -15,9 +15,11 @@ class DashboardController extends Controller
     public function index()
     {
        if(Auth::check()){
+        $total_deposits = Transaction::where('txn_amount', '>', 0)->where('status', '=', 'SUCCESS')->sum('txn_amount');
         $stats = [
             'customers'=>Customer::count(),
-            'transactions'=>Transaction::count()
+            'transactions'=>Transaction::count(),
+            'total_deposits'=>number_format($total_deposits, 2)
         ];
         //Get all users
         $users = User::from('users AS u')->join('roles AS r', 'u.role','=','r.id')->get([
--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -29,9 +29,9 @@
                             </div>
                             <div class="col-xl-3 col-md-6">
                                 <div class="card bg-success text-white mb-4">
-                                    <div class="card-body">MK4,245,000</div>
+                                    <div class="card-body">MK{{$stats['total_deposits']}}</div>
                                     <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="#">In Deposits</a>
+                                        <a class="small text-white stretched-link" href="#">Total Deposits</a>
                                         <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                                     </div>
                                 </div>
